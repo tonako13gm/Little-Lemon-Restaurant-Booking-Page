53474d6e8af44d89613cf9d126ba03945cda27af
Anthony Sylvester Bahinting <anthony.b@aware.co.th>
2024-07-25 10:18:58 +0800

    added basic layout, logo and nav at the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,33 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Flex, Image } from '@chakra-ui/react'
+import logo from '../assets/logo.jpg'
+import TestLink from "../components/TestLink";
+import { Routes, Route, Link } from "react-router-dom";
+
+
 
 function Header() {
     return (
         <Flex as='header' border='1px' borderColor='BlackAlpha.900' w='100%' p={4} color='BlackAlpha.900'>
-            <Box flex='2' border='1px' borderColor='BlackAlpha.900' h={3}></Box>
-            <Box flex='2' border='1px' borderColor='BlackAlpha.900' h={3}></Box>
-            <Box flex='6' border='1px' borderColor='BlackAlpha.900' h={3}></Box>
-            <Box flex='2' border='1px' borderColor='BlackAlpha.900' h={3}></Box>
+            <Box flex='2'></Box>
+            <Box flex='2'>
+                <Image src={logo} alt='Little Lemmon' />
+            </Box>
+            <Box as='nav'flex='6' alignContent="center">
+                <Flex direction="row" justifyContent="space-around">
+                    <Link to="/">Home</Link>
+                    <Link to="/">About</Link>
+                    <Link to="/">Menu</Link>
+                    <Link to="/">Reservations</Link>
+                    <Link to="/">Order Online</Link>
+                    <Link to="/">Login</Link>
+                </Flex>
+                <Routes> 
+                    <Route path="../components/TestLink" element={<TestLink />}></Route>
+                    {/* <Route path="/about" element={<AboutLittleLemon />}></Route>
+                    <Route path="/contact" element={<Contact />}></Route> */}
+                </Routes> 
+            </Box>
+            <Box flex='2'></Box>
         </Flex>
     )
   }
---
+++ src/components/TestLink.js
@@ -0,0 +1,4 @@
+function TestLink() {
+    return  <h1>This is a test link page</h1>
+}
+export default TestLink
